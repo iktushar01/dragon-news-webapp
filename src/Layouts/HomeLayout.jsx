c6d2e5f1cc350d3f8ea2c9dcabfe5bed3bf90195
tushar <iktushar2020@gmail.com>
2025-05-01 15:32:51 +0600

    added github and google icon from daisyUI

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -5,6 +5,7 @@ import LatestNews from "../Components/LatestNews";
 import Navbar from "../Components/Navbar";
 import LeftAside from "../Components/HomeLayout/LeftAside";
 import RightAside from "../Components/HomeLayout/RightAside";
+import SocialLogin from "../Components/HomeLayout/SocialLogin";
 
 const HomeLayout = () => {
   return (
@@ -24,7 +25,7 @@ const HomeLayout = () => {
           <Outlet />
         </section>
         <aside className="col-span-3">
-          <RightAside />
+          <SocialLogin></SocialLogin>
         </aside>
       </main>
     </div>
--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const  {id} = useParams();
+    console.log(id)
     return (
         <div>
-            <h1>wow</h1>
+            <h1 className='font-bold'>Dragon News Home </h1>
         </div>
     );
 };
